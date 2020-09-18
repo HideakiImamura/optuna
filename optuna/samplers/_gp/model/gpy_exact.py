7ee96f421956250808a959ccf6a0b6bc214fad34
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-09-18 17:36:04 +0900

    Fix outlier detectoin method

--- optuna/samplers/_gp/model/gpy_exact.py
+++ optuna/samplers/_gp/model/gpy_exact.py
@@ -250,9 +250,9 @@ class GPyExact(BaseModel):
     def _normalize(x: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         y = y.flatten()
         q = np.quantile(y, [0.25, 0.75])
-        indexes = (0.75 * q[0] - 0.25 * q[1] <= y) * (y <= 0.25 * q[0] + 1.25 * q[1])
+        indexes = y <= 0.25 * q[0] + 1.25 * q[1]
         if any(indexes):
-            print("Removed outliers!")
+            print("Removed outliers: {}/{}".format(np.sum(~indexes), len(indexes)))
             x = x[indexes]
             y = y[indexes]
         y = (y - y.mean()) / y.std()
